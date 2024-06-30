5060cca7c4d32a621a2800c0934b36f6c2a80532
Vlad Skvortsov <vss@73rus.com>
2024-06-30 17:13:52 +0300

    Update label on slider drag

--- side_panel.gd
+++ side_panel.gd
@@ -1,5 +1,13 @@
 extends VBoxContainer
 
 
+var _cells_per_player = 9
+
+
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_cells_per_player_value_changed(value):
+	_cells_per_player = value
+	%CellsPerPlayer.text = str(value)
