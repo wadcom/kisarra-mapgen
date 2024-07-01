2fbd9a31046d91e6a9be123e418e652b846d3148
Vlad Skvortsov <vss@73rus.com>
2024-07-01 10:13:28 +0300

    Extract _setup_ground_cells()

--- map_area.gd
+++ map_area.gd
@@ -10,8 +10,17 @@ const _globals = preload("res://globals.gd")
 func update_parameters(params):
 	custom_minimum_size = Vector2.ONE * _globals.PIXELS_PER_CELL_SIDE * params.map_size
 
-	for c in $Cells.get_children():
-		$Cells.remove_child(c)
+	_setup_ground_cells(params)
+
+	for i in params.players_qty:
+		var base = base_cell_prefab.instantiate()
+		base.position = Vector2(i, 0) * _globals.PIXELS_PER_CELL_SIDE
+		$Bases.add_child(base)
+
+
+func _setup_ground_cells(params):
+	for c in $GroundCells.get_children():
+		$GroundCells.remove_child(c)
 		c.queue_free()
 
 	for x in params.map_size:
@@ -19,9 +28,4 @@ func update_parameters(params):
 			var cell = sand_cell_prefab.instantiate()
 
 			cell.position = Vector2(x, y) * _globals.PIXELS_PER_CELL_SIDE
-			$Cells.add_child(cell)
-
-	for i in params.players_qty:
-		var base = base_cell_prefab.instantiate()
-		base.position = Vector2(i, 0) * _globals.PIXELS_PER_CELL_SIDE
-		$Bases.add_child(base)
+			$GroundCells.add_child(cell)
