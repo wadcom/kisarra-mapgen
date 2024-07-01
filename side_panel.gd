78a8c86af20ab70ec0416e10e6c3fafcf6aaf80b
Vlad Skvortsov <vss@73rus.com>
2024-07-01 06:55:41 +0300

    Emit signal when parameters change

--- side_panel.gd
+++ side_panel.gd
@@ -1,15 +1,18 @@
 extends VBoxContainer
 
+signal parameters_changed(params: Variant)
 
-var _cells_per_player = 120
-var _map_size = 22
-var _players_qty = 2
+var _params = {
+	cells_per_player = 120,
+	map_size = 22,
+	players_qty = 2,
+}
 
 
 func _ready():
-	%CellsPerPlayer.value = _cells_per_player
-	%MapSize.value = _map_size
-	%PlayersQuantity.value = _players_qty
+	%CellsPerPlayer.value = _params.cells_per_player
+	%MapSize.value = _params.map_size
+	%PlayersQuantity.value = _params.players_qty
 
 	_update_labels()
 
@@ -19,22 +22,25 @@ func _on_quit_button_pressed():
 
 
 func _on_cells_per_player_value_changed(value):
-	_cells_per_player = value
+	_params.cells_per_player = value
 	_update_labels()
+	parameters_changed.emit(_params)
 
 
 func _on_map_size_value_changed(value):
-	_map_size = value
+	_params.map_size = value
 	_update_labels()
+	parameters_changed.emit(_params)
 
 
 func _on_players_quantity_value_changed(value):
-	_players_qty = value
+	_params.players_qty = value
 	_update_labels()
+	parameters_changed.emit(_params)
 
 
 func _update_labels():
-	%CellsPerPlayerLabel.text = str(_cells_per_player)
-	%MapSizeLabel.text = str(_map_size)
+	%CellsPerPlayerLabel.text = str(_params.cells_per_player)
+	%MapSizeLabel.text = str(_params.map_size)
 
-	%RecommendedMapSizeLabel.text = str(ceil(sqrt(_players_qty * _cells_per_player)))
+	%RecommendedMapSizeLabel.text = str(ceil(sqrt(_params.players_qty * _params.cells_per_player)))
