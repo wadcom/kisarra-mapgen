2cd1416bfbcfab349c5eab333790eddd2f865a38
Vlad Skvortsov <vss@73rus.com>
2024-07-01 06:28:52 +0300

    Add map size controls

--- side_panel.gd
+++ side_panel.gd
@@ -1,6 +1,7 @@
 extends VBoxContainer
 
 
+var _map_size = 22
 var _cells_per_player = 9
 
 
@@ -11,3 +12,8 @@ func _on_quit_button_pressed():
 func _on_cells_per_player_value_changed(value):
 	_cells_per_player = value
 	%CellsPerPlayer.text = str(value)
+
+
+func _on_map_size_value_changed(value):
+	_map_size = value
+	%MapSizeLabel.text = str(value)
