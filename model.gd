d697ff63043214f00021c5a0ff407e9e34bb2783
Vlad Skvortsov <vss@73rus.com>
2024-10-14 09:44:27 +0300

    Extract make_surface()

--- model.gd
+++ model.gd
@@ -8,22 +8,7 @@ enum SurfaceType { MOUNTAINS, SAND }
 
 
 func setup_surface(params, height_map):
-	_surface = Array()
-	_surface.resize(params.map_size)
-
-	for x in params.map_size:
-		_surface[x] = Array()
-		_surface[x].resize(params.map_size)
-
-		for y in params.map_size:
-			var cell = {}
-			if _is_mountain(params, height_map, Vector2i(x, y)):
-				cell = { type = SurfaceType.MOUNTAINS }
-			else:
-				cell = { type = SurfaceType.SAND }
-
-			_surface[x][y] = cell
-
+	_surface = make_surface(params, height_map)
 	surface_updated.emit()
 
 
@@ -34,3 +19,23 @@ func get_surface():
 func _is_mountain(params, height_map, p: Vector2i):
 	var idx = p.y * params.map_size + p.x
 	return height_map[idx] > params.mountains.height_threshold
+
+
+func make_surface(params, height_map):
+	var surface = Array()
+	surface.resize(params.map_size)
+
+	for x in params.map_size:
+		surface[x] = Array()
+		surface[x].resize(params.map_size)
+
+		for y in params.map_size:
+			var cell = {}
+			if _is_mountain(params, height_map, Vector2i(x, y)):
+				cell = { type = SurfaceType.MOUNTAINS }
+			else:
+				cell = { type = SurfaceType.SAND }
+
+			surface[x][y] = cell
+
+	return surface
